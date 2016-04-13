2e461ebfb8b1adb8608e71a0637286666c025cb3
LeeBry <Echoslee@yahoo.com>
2016-04-12 21:15:29 -0500

    Hehe, working on it earlier to get a start ;D

---
+++ TheMenuGame.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class TheMenuGame here.
+ * 
+ * @author Bryce Lee
+ * @version 1.0
+ */
+public class TheMenuGame
+{
+    // instance variables - replace the example below with your own
+    private int gold;
+    private int health;
+    /**
+     * Constructor for objects of class TheMenuGame
+     */
+    public TheMenuGame()
+    {
+        gold = 0;
+        health= 100;
+    }
+
+    
+}
