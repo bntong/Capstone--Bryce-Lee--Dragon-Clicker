92b456270992bbc600c579d5b758c232bab789ce
LeeBry <Echoslee@yahoo.com>
2016-04-25 14:21:59 -0500

    Level Up Button
    Need Monsters now.

--- ButtonClicker.java
+++ ButtonClicker.java
@@ -8,38 +8,66 @@ public class ButtonClicker
     private static final int FRAME_WIDTH= 350;// in pixels
     private static final int FRAME_HEIGHT= 500;
     private String action;
+    // 
     private int health;
     private int coins;
     private int kills;
+    private int tempkills;
+    private int multiplier;
+    private int damage;
+    private int needed; //coins for levelup
+    // Variables 
     private JFrame frame;// Frames can only have one component
-    private JButton button;
-    private JButton button2;
+    // Frame^^^^^
+    private JButton buttonAttack;
+    private JButton buttonCollect;
+    private JButton buttonLevelUp;
+    // Buttons^^^^^
     private JPanel panel;
     private JPanel panelTop;
+    // Panels^^^^^
     private JLabel label;
-
+    private JLabel label2;
+    // Labels^^^^^
+    private JPanel monsterPanel;
+    
     public ButtonClicker()
     {
         this.health= 100;
+        this.coins= 0;
+        this.kills=0;
+        this.tempkills=0;
+        this.multiplier=1;
+        this.damage= 5;
+        this.needed= 5;
+        // variables
         this.frame= new JFrame();
         this.panel= new JPanel();
         this.panelTop= new JPanel();
+        this.monsterPanel= new JPanel();
         this.label= new JLabel();
+        this.label2= new JLabel();
         BorderLayout layout= new BorderLayout();;
         
-        this.button= new JButton("Attack");
-
+        this.buttonAttack= new JButton("Attack");
+        this.buttonCollect= new JButton("Collect");
+        this.buttonLevelUp= new JButton("Level Up");
+        // Making the buttons
         this.panelTop.add(this.label,layout.NORTH);
-        this.panel.add(this.button);
+        this.panelTop.add(this.label2,layout.SOUTH);
+  
         // We put the buttons in the panel, then we add it to frame
-        this.button2= new JButton("Collect");
-        this.panel.add(this.button2);
+        this.panel.add(this.buttonAttack);
+        this.panel.add(this.buttonCollect);
+        this.panel.add(this.buttonLevelUp);
+        //
         this.frame.add(panelTop,layout.NORTH);
         this.frame.add(panel,layout.SOUTH);
-
+        
         ClickListener listener= new ClickListener();
-        button.addActionListener(listener);
-        button2.addActionListener(listener);
+        buttonAttack.addActionListener(listener);
+        buttonCollect.addActionListener(listener);
+        buttonLevelUp.addActionListener(listener);
 
         this.frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         this.frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
@@ -59,19 +87,38 @@ public class ButtonClicker
             action=event.getActionCommand();
             if(action.equals("Attack"))
             {
-              health-= 5;
+              health-= damage;
               if (health<=0)
                 {
                 health=100;
                 kills++;
+                tempkills= tempkills+1*multiplier;
+                if(kills%5==0)
+                {
+                    multiplier++;
+                }
                }
-              label.setText("HP is now" + health);
+              label.setText("HP: " + health+"\t"+" Coins: " +coins+" Kills: "+kills);
+              label2.setText("You need: "+ needed+" coins to level");
             }
             if(action.equals("Collect"))
             {
-                coins+=kills;
+                coins+=tempkills;
+                tempkills=0;
+                label.setText("HP: " + health+"\t"+" Coins: " +coins+" Kills: "+kills);
+                label2.setText("You need: "+ needed+" coin to level");
             }
-            
+            if (action.equals("Level Up"))
+            {
+                if (coins >= needed)
+                {
+                    damage++;
+                    coins-=needed;
+                    needed+=5;
+                    label.setText("HP: " + health+"\t"+" Coins: " +coins+" Kills: "+kills);
+                    label2.setText("You need: "+ needed+" coin to level");
+                }
+            }
         }
 
     }
---
+++ MonsterDisplayer.java
@@ -0,0 +1,21 @@
+import javax.swing.*; // JFrame, JButton, JPanel, JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
+/**
+ * Write a description of class Monsters here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MonsterDisplayer
+{
+    
+    /**
+     * Constructor for objects of class Monsters
+     */
+    public MonsterDisplayer()
+    {
+        
+    }
+}
